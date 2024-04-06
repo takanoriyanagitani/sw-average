aedb0da644b9477196cefa7e0df5e2c06d5cca0f
yanagitani <takanori.yanagitani@gmail.com>
2024-04-07 07:59:04 +0900

    test: trying to cover using darwin(wip)

--- cov.sh
+++ cov.sh
@@ -1,26 +1,30 @@
 #!/bin/sh
 
 numcpus=23
-jobs=$(( ${numcpus} - 1 ))
+jobs=$((${numcpus} - 1))
 
-swift \
-    test \
-    --quiet \
-    --parallel \
-    --enable-code-coverage \
-    --jobs ${jobs} \
-    || echo NG
+covLinux() {
+	swift \
+		test \
+		--quiet \
+		--parallel \
+		--enable-code-coverage \
+		--jobs ${jobs} ||
+		echo NG
 
-llvm-cov \
-    export \
-    -format=lcov \
-    ./.build/debug/sw-averagePackageTests.xctest \
-    -instr-profile ./.build/debug/codecov/default.profdata |
-    cat > ./cov.lcov
+	llvm-cov \
+		export \
+		-format=lcov \
+		./.build/debug/sw-averagePackageTests.xctest \
+		-instr-profile ./.build/debug/codecov/default.profdata |
+		cat >./cov.lcov
 
-llvm-cov \
-    report \
-    --ignore-filename-regex=.build \
-    --summary-only \
-    ./.build/debug/sw-averagePackageTests.xctest \
-    -instr-profile ./.build/debug/codecov/default.profdata
+	llvm-cov \
+		report \
+		--ignore-filename-regex=.build \
+		--summary-only \
+		./.build/debug/sw-averagePackageTests.xctest \
+		-instr-profile ./.build/debug/codecov/default.profdata
+}
+
+covLinux
